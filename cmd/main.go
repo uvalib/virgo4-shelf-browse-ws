fb725c4dd1d0823b01954c188236f1905495bd84
Dave Goldstein <dpg3k@virginia.edu>
2021-11-29 12:18:49 -0500

    VIRGONEW-2371: remove Prometheus support

--- cmd/main.go
+++ cmd/main.go
@@ -7,9 +7,6 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-	ginprometheus "github.com/zsais/go-gin-prometheus"
 )
 
 /**
@@ -34,15 +31,18 @@ func main() {
 	corsCfg.AddAllowHeaders("Authorization")
 	router.Use(cors.New(corsCfg))
 
-	p := ginprometheus.NewPrometheus("gin")
+	//
+	// we are removing Prometheus support for now
+	//
+	//p := ginprometheus.NewPrometheus("gin")
 
 	// roundabout setup of /metrics endpoint to avoid double-gzip of response
-	router.Use(p.HandlerFunc())
-	h := promhttp.InstrumentMetricHandler(prometheus.DefaultRegisterer, promhttp.HandlerFor(prometheus.DefaultGatherer, promhttp.HandlerOpts{DisableCompression: true}))
+	//router.Use(p.HandlerFunc())
+	//h := promhttp.InstrumentMetricHandler(prometheus.DefaultRegisterer, promhttp.HandlerFor(prometheus.DefaultGatherer, promhttp.HandlerOpts{DisableCompression: true}))
 
-	router.GET(p.MetricsPath, func(c *gin.Context) {
-		h.ServeHTTP(c.Writer, c.Request)
-	})
+	//router.GET(p.MetricsPath, func(c *gin.Context) {
+	//	h.ServeHTTP(c.Writer, c.Request)
+	//})
 
 	router.GET("/favicon.ico", svc.ignoreHandler)
 
